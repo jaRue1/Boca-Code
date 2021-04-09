3501b585ef97d8bc14cdd93e507be8b06333fb44
jaRue1 <jaruejohnson@outlook.com>
2021-04-08 23:19:47 -0400

    Cleaned up some code and made sure it all works, practiced troubleshooting issues

--- CLASSCODE/OBJECTS/objectsPractice.js
+++ CLASSCODE/OBJECTS/objectsPractice.js
@@ -1,3 +1,4 @@
+// object example
 const person = {
   firstName: "Rue",
   lastName: "Johnson",
@@ -7,56 +8,55 @@ const person = {
     state: "Florida",
     zipcode: "33433",
   },
-  pets: ['😸','🐶','🐎'],
+  pets: ["😸", "🐶", "🐎"],
 }
-//add properties to an object 
-person.family = ['Blue','Rue','Jones','Green']
+//add properties to an object
+person.family = ["Blue", "Rue", "Jones", "Greene"];
 
 // adding element to an array
-person.pets.push('🐟')
+person.pets.push("🐟");
 
-// assigning object properties values to variables
-const myAge = person.age 
-const myCity = person.address.city
-const myZip = person.address.zip
-const myState = person.address.state
-//console.log("My state is "+ myState)
+// assigning object properties values to variables (this is the long way of doing this)
+const myAge = person.age;
+const myCity = person.address.city;
+const myZip = person.address.zip;
+const myState = person.address.state;
+console.log("My state is " + myState);
 
-// Destructuring Nested Object Address
-const {zip , state, city} = person
+// Destructuring Object Person (DO THIS !) 
+const { address, age, firstName, lastName, pets } = person;
 
-// Destructuring Object Person
-const {address, age, firstName,lastName, pets} = person
+// Destructuring Nested Object Address (DO THIS TOO!)
+const { zip, state, city } = person;
 
-//template literal 
-// console.log(`My city ${address.city}, my zipcode ${address.zipcode}, and my age is ${age}`)
-
+//template literal
+console.log(
+  `My city ${address.city}, my zipcode ${address.zipcode}, and my age is ${age}`
+)
+//---------------------------------------------------------------------------------------------
 // function that calculate the users birth year using age value
-function getYear(myAge){
+function getYear(myAge) {
   const thisYear = 2021
   let birthYear = thisYear - myAge
-  //console.log(birthYear)
   return birthYear
 }
-
-
-const myYear = getYear(age)
-//console.log('my year hear after the function', myYear)
-//console.log('These are all object keys '+ Object.keys(person))
+//--------------------------------------------------------
+const myYear = getYear(age);
+console.log("my year hear after the function", myYear);
+console.log("These are all object keys " + Object.keys(person))
 
 const allKeys = Object.keys(person)
-//console.log('this is an array of keys ' + allKeys)
+console.log("this is an array of keys " + allKeys)
+//--------------------------------------------------------
 
+//Three ways of writing the same how to display object value.
+console.log(person.firstName)
+console.log(person["firstName"])
+console.log(person[allKeys[0]])
 
-//Three ways of writing the same how to display object value. 
-// console.log(person.firstName)
-// console.log(person['firstName'])
-// console.log(person[allKeys[0]])
+// assigning the values of the object person to variable all person.
+const allValues = Object.values(person)
+console.log("This is all the value " + allValues)
 
-// assigning the values of the object person to varialble all person.
-const allValues = Object.values(person);
-//console.log('This is all the value ' + allValues)
-
-// display pets array 
+// display pets array
 console.log(person.pets)
-
