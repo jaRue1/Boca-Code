78ac80f68b4ba32311581e740adfe6f26f68c51e
jaRue1 <jaruejohnson@outlook.com>
2021-04-17 21:08:25 -0400

    updated my class example it still not complete

--- CLASSCODE/OBJECTS/classes.js
+++ CLASSCODE/OBJECTS/classes.js
@@ -3,11 +3,11 @@ class Product{
     this.name = prodName
   }
   display(){
-    return`This is , ${this.name} `
+    return`This is a ${this.name} `
   }
 }
 class Item extends Product{
-  constructor(prodName, sku, style , qty ){
+  constructor(prodName, sku, style , qty){
   super(prodName)
   this.sku = sku
   this.style = style
@@ -25,8 +25,16 @@ class Item extends Product{
 }
 
 let prod1 = new Product('Shirt')
+console.log("----------Parent Class Object-----------")
 prod1.display()
+console.log(prod1.display())
 let item1 = new Item('Ski Mask','TKL','Large', 10)
+console.log("----------Child Class Object-----------")
 item1.sellSome(3)
 item1.buySome(4)
 item1.itemDisplay()
+console.log("---------- Item Attributes -----------")
+//why is this undefined?
+console.log("item name  ",item1.prodName ," Derived from the parent class")
+console.log("item style " ,item1.style)
+console.log("item qty   ",item1.qty)
