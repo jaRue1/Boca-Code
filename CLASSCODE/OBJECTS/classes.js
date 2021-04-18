536c383af3a36da67220d5bb1bcdb2b6f3dca5df
jaRue1 <jaruejohnson@outlook.com>
2021-04-18 10:12:46 -0400

    Fixed the issue with todd

--- CLASSCODE/OBJECTS/classes.js
+++ CLASSCODE/OBJECTS/classes.js
@@ -1,40 +1,43 @@
-class Product{
-  constructor(prodName){
+class Product {
+  constructor(prodName) {
     this.name = prodName
   }
-  display(){
-    return`This is a ${this.name} `
+  display() {
+    return `This is a ${this.name} `
+  }
+  getProdName(){
+    return this.name
   }
 }
-class Item extends Product{
-  constructor(prodName, sku, style , qty){
-  super(prodName)
-  this.sku = sku
-  this.style = style
-  this.qty = qty
+class Item extends Product {
+  constructor(prodName, sku, style, qty) {
+    super(prodName)
+    this.sku = sku
+    this.style = style
+    this.qty = qty
   }
-  itemDisplay(){
-    console.log(this.display() + this.sku +' '+ this.qty)
+  itemDisplay() {
+    console.log(this.display() + this.sku + " " + this.qty)
   }
-  sellSome(qtySold){
+  sellSome(qtySold) {
     this.qty -= qtySold
   }
-  buySome(qtyBuy){
+  buySome(qtyBuy) {
     this.qty += qtyBuy
   }
 }
 
-let prod1 = new Product('Shirt')
+let prod1 = new Product("Shirt")
 console.log("----------Parent Class Object-----------")
 prod1.display()
 console.log(prod1.display())
-let item1 = new Item('Ski Mask','TKL','Large', 10)
+let item1 = new Item("Ski Mask", "TKL", "Large", 10)
 console.log("----------Child Class Object-----------")
 item1.sellSome(3)
 item1.buySome(4)
 item1.itemDisplay()
 console.log("---------- Item Attributes -----------")
 //why is this undefined?
-console.log("item name  ",item1.prodName ," Derived from the parent class")
-console.log("item style " ,item1.style)
-console.log("item qty   ",item1.qty)
+console.log("item name  ", item1.getProdName(), " Derived from the parent class")
+console.log("item style ", item1.style)
+console.log("item qty   ", item1.qty)
