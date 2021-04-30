1e53247e37d597956a1680fb376f640d16e11b1f
jaRue1 <jaruejohnson@outlook.com>
2021-04-30 02:11:00 -0400

    fixed it !

--- HW/hw01.js
+++ HW/hw01.js
@@ -2,15 +2,17 @@
 // Write a function that REMOVES items from shopping list that are IN THE CART
 let shoppingList = ['Milk','Chicken','Pineapple','Water','Bread','Eggs','Butter','Shampoo']
 let myCart = ['Milk','Chicken','Pineapple','Water','Bread']
-// Version 1 ----------------------------------------------
-function addTo(shop,cart) {
-  shop.filter((shop) => {
-    return !cart.includes(shop)
+//Version 1 -------------------------------------------------------
+function addTo(shoppingList,myCart) {
+ let leftOver = shoppingList.filter((shoppingList) => {
+    return !myCart.includes(shoppingList)
   })
+  return leftOver
 }
-console.log(addTo(shoppingList,myCart))
-//---------------------------------------------------------
-// Version 2 ----------------------------------------------
-const addToCart = shoppingList.filter((data) => {return !cart.includes(data)})
+let result = addTo(shoppingList,myCart)
+console.log(result)
+//-------------------------------------------------------------------
+// Version 2 --------------------------------------------------------
+const addToCart = shoppingList.filter((data) => {return !myCart.includes(data)})
 console.log(addToCart)
-//---------------------------------------------------------
+//-------------------------------------------------------------------
