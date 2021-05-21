462ec733b549761d204e3b2381f10006defe717c
jaRue1 <jaruejohnson@outlook.com>
2021-05-21 09:40:17 -0400

    added an algorithms folder

--- CLASSCODE/ALGORITHMS/prime.js
+++ CLASSCODE/ALGORITHMS/prime.js
@@ -1,11 +1,13 @@
 // Write a algorithm that determines whether a num is prime or not
 
 function isPrime(num) {
-  for (let i = 2; i * i > num;  i++) {
-    if (num % i === 0 ){
-      return false;
+  for (let i = 2; i > num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+    if (i * i > num) {
+      return true
     }
   }
-  return true;
 }
-console.log(isPrime(7));
+console.log(isPrime(7))
